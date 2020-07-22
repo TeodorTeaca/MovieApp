903c1d0d4838af8185460e8507d611787484d0bf
TeodorTeaca <68395102+TeodorTeaca@users.noreply.github.com>
2020-07-22 14:56:53 +0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 "use strict"
-
 angular.module('MovieApp', []);
-angular.module('MovieApp').controller('MainController', ['$scope', function ($scope) {
-    console.log('hello');
-}]);
+
+angular.module('MovieApp').controller('FetchController', [function () {
+    this.nowPlayingURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=d8bf019d0cca372bd804735f172f67e8";
+
+    this.getNowPlayingMovie = function () {
+        fetch(this.nowPlayingURL)
+            .then((res) => res.json())
+            .then((data) => this.getData(data.results))
+            .catch((error) => { console.log(error); });
+    }
+
+    this.getData = function (data) {
+        this.arrayObjects = data;
+        console.log(data);
+    }
+
+}])
+
+
+angular.module('MovieApp').controller('UpcomingController', [function () {
+    this.upcomingURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=d8bf019d0cca372bd804735f172f67e8";
+
+    this.getNowPlayingMovie = function () {
+        fetch(this.upcomingURL)
+            .then((res) => res.json())
+            .then((data) => this.getUpcoming(data.results))
+            .catch((error) => { console.log(error); });
+    }
+
+    this.getUpcoming = function (data) {
+        this.arrayObjects = data;
+        console.log(data);
+    }
+
+}])
