4c4a01b904f66e454183aad60cef426104445743
TeodorTeaca <68395102+TeodorTeaca@users.noreply.github.com>
2020-07-31 09:39:16 +0300

    refactor

--- app/routing.js
+++ app/routing.js
@@ -1,7 +1,4 @@
 angular.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
-    $routeProvider.when('/movies', {
-        templateUrl: 'views/movie.list.html',
-    })
-        .otherwise({ redirectTo: '/login' });
+    $routeProvider.otherwise({ redirectTo: '/login' });
 }]);
