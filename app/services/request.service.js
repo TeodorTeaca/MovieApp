a492fe338f5eb72518960a0b0360a5eaceabab75
TeodorTeaca <68395102+TeodorTeaca@users.noreply.github.com>
2020-08-03 13:32:28 +0300

    commit

---
+++ app/services/request.service.js
@@ -0,0 +1,10 @@
+angular.module('Angular.movies').service('ServiceMovies', function ($http) {
+    return {
+        request: function (path) {
+            return $http.get(`https://api.themoviedb.org/3/movie/${path}?api_key=d8bf019d0cca372bd804735f172f67e8`)
+                .then((response) => {
+                    return response.data.results;
+                })
+        }
+    }
+});
